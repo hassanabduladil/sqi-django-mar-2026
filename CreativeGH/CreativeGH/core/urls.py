11d9c1823295a9ac5d1548f584ea023a7080356d
Abdul-Adil <hassanabduladil@gmail.com>
2026-03-12 04:37:23 -0700

    idk

--- CreativeGH/CreativeGH/core/urls.py
+++ CreativeGH/CreativeGH/core/urls.py
@@ -5,5 +5,7 @@ app_name = "core"
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('services', views.services, name='services')
+    path('services', views.services, name='services'),
+    path('portfolio', views.portfolio, name='portfolio'),
+    path('contact', views.contact, name='contact'),
 ]
--- CreativeGH/CreativeGH/core/views.py
+++ CreativeGH/CreativeGH/core/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def index(request):
@@ -7,3 +6,9 @@ def index(request):
 
 def services(request):
     return render(request, 'core/services.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
